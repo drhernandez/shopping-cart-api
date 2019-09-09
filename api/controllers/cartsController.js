97ac95a43417195272553189bed832746b96b88a
Diego Hernández <drhernandez92@gmail.com>
2019-09-08 21:53:06 -0300

    added get, put y delete methods on cart service

--- api/controllers/cartsController.js
+++ api/controllers/cartsController.js
@@ -1,13 +1,30 @@
 const logger = require('../utils/loggerFactory').createLogger(__filename);
 const to = require('await-to-js').default;
 const { Response } = require('../models');
-const { ApiError, InternalError, BadRequestError } = require('../errors');
+const { ApiError, InternalError, BadRequestError, NotFoundError } = require('../errors');
 const { CartsService } = require('../services');
 
 class CartsController {
   
   async getCartById(req, res) {
-    res.status(200).json({});
+    
+    const cartId = req.params.cart_id;
+    let response = {};
+
+    const [err, cart] = await to(CartsService.getCartById(cartId));
+    if (err && err instanceof ApiError) {
+      logger.error(`[message: Error getting cart] [error: ${JSON.stringify(err)}]`);
+      response = new Response(err.status, err);
+    } else if (err) {
+      logger.error(`[message: Error getting cart] [error: ${err.message}]`);
+      response = new Response(500, new InternalError());
+    } else if (!cart) {
+      response = new Response(404, new NotFoundError());
+    } else {
+      response = new Response(200, cart);
+    }
+
+    res.status(response.status).json(response.body);
   }
 
   async createCart(req, res) {
@@ -38,16 +55,16 @@ class CartsController {
     }
 
     if (causes.length > 0) {
-      logger.error(`[message: Error trying to create a cart] [error: invalid body] [causes: ${JSON.stringify(causes)}]`)
+      logger.error(`[message: Error creating a cart] [error: invalid body] [causes: ${JSON.stringify(causes)}]`)
       response = new Response(400, new BadRequestError('invalid body', causes));
     } else {
 
       const [err, cart] = await to(CartsService.createCart(req.body));
       if (err && err instanceof ApiError) {
-        logger.error(`[message: Error trying to create a new cart] [error: ${JSON.stringify(err)}]`);
+        logger.error(`[message: Error creating a new cart] [error: ${JSON.stringify(err)}]`);
         response = new Response(err.status, err);
       } else if (err) {
-        logger.error(`[message: Error trying to create a new cart] [error: ${err.message}]`);
+        logger.error(`[message: Error creating a new cart] [error: ${err.message}]`);
         response = new Response(500, new InternalError());
       } else {
         response = new Response(201, cart);
@@ -58,11 +75,64 @@ class CartsController {
   }
 
   async updateCart(req, res) {
-    res.status(200).send('updated');
+    
+    const cartId = req.params.cart_id;
+    let causes = [];
+    let response = {};
+
+    if (req.body.cart_items && !req.body.cart_items.length) {
+      causes.push('cart_items must be an array and must have at least one item')
+    } else if (req.body.cart_items){
+      req.body.cart_items.forEach(item => {
+        if (item.variant_id == undefined) {
+          causes.push(`missing variant_id on cart_item: ${JSON.stringify(item)}`);
+        }
+        if (item.quantity == undefined) {
+          causes.push(`missing quantity on cart_item: ${JSON.stringify(item)}`);
+        }
+        if (isNaN(item.quantity) || item.quantity < 0) {
+          causes.push(`quantity must be a positive number on item: ${JSON.stringify(item)}`);
+        }
+      });
+    }
+
+    if (causes.length > 0) {
+      logger.error(`[message: Error updating cart ${cartId}] [error: invalid body] [causes: ${JSON.stringify(causes)}]`)
+      response = new Response(400, new BadRequestError('invalid body', causes));
+    } else {
+
+      const [err, cart] = await to(CartsService.updateCart(cartId, req.body));
+      if (err && err instanceof ApiError) {
+        logger.error(`[message: Error updating cart ${cartId}] [error: ${JSON.stringify(err)}]`);
+        response = new Response(err.status, err);
+      } else if (err) {
+        logger.error(`[message: Error updating cart ${cartId}] [error: ${err.message}]`);
+        response = new Response(500, new InternalError());
+      } else {
+        response = new Response(200, cart);
+      }
+    }
+
+    res.status(response.status).json(response.body);
   }
 
   async deleteCart(req, res) {
-    res.status(204).end();
+    
+    const cartId = req.params.cart_id;
+    let response = {};
+
+    const [err] = await to(CartsService.deleteCartById(cartId));
+    if (err && err instanceof ApiError) {
+      logger.error(`[message: Error deleting cart] [error: ${JSON.stringify(err)}]`);
+      response = new Response(err.status, err);
+    } else if (err) {
+      logger.error(`[message: Error deleting cart] [error: ${err.message}]`);
+      response = new Response(500, new InternalError());
+    } else {
+      response = new Response(204);
+    }
+
+    res.status(response.status).json(response.body);
   }
 }
 
--- api/db/models/cartitem.js
+++ api/db/models/cartitem.js
@@ -1,20 +1,20 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  
-  const CartItem = sequelize.define('CartItem', {
-    variant_id: { type: DataTypes.BIGINT, allowNull: false },
-    quantity: { type: DataTypes.INTEGER, allowNull: false }
-  }, {
-      indexes: [{
-        unique: true,
-        fields: ['variant_id', 'cart_id']
-      }]
-  });
-  
-  CartItem.associate = function (models) {
-    // associations can be defined here
-    CartItem.belongsTo(models.Cart);
-  };
+const sequelize = require('sequelize');
 
-  return CartItem;
-};
+class CartItem extends sequelize.Model {
+
+  static init(sequelize, DataTypes) {
+    return super.init(
+      {
+        variant_id: { type: DataTypes.BIGINT, allowNull: false },
+        quantity: { type: DataTypes.INTEGER, allowNull: false }
+      },
+      { sequelize }
+    );
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Cart);
+  }
+}
+
+module.exports = CartItem;
